f04f71deb1c24edc65c646994a54a2ec959d40ad
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-07 23:35:36 -0400

    Null-aware operators

---
+++ bin/null_aware _operators.dart
@@ -0,0 +1,19 @@
+String foo = 'a string';
+String bar;
+String baz = foo ?? bar;
+
+void main(){
+  //The initial value of a is null
+  int a;
+  a ??= 3;
+  print(a);
+  //The change vale of a not null
+  a ??= 5;
+  print(a);
+  //Validation
+  print(1 ?? 3);
+  print(a ??  12);
+  //Challenge
+  bar ??= 'a string';
+  print(bar);
+}
