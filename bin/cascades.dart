21193e35253d700fe5120d282b431ec00f6f2438
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-10 23:49:47 -0400

    Cascades

---
+++ bin/cascades.dart
@@ -0,0 +1,25 @@
+//Big Object
+class BigObject {
+  int anInt = 0;
+  String aString = '';
+  List<double> aList = [];
+  bool _done = false;
+
+  void allDone() {
+    _done = true;
+  }
+}
+
+//Challenge
+BigObject fillBigObject(BigObject obj) {
+  return obj
+    ..anInt = 1
+    ..aString = 'String!'
+    ..aList.add(3)
+    ..allDone();
+}
+
+void main() {
+  var bigObject = BigObject();
+  print(fillBigObject(bigObject));
+}
