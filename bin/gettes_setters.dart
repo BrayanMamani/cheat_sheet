cee7b649981bcd0fd25f1c8ed67bf7b7857c85c1
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-10 23:56:02 -0400

    Getters and setters

---
+++ bin/gettes_setters.dart
@@ -0,0 +1,17 @@
+//Challenge
+class ShoppingCart {
+  List<double> _prices = [];
+
+  List<double> get prices => _prices;
+
+  set prices(List<double> value) {
+    _prices = value;
+  }
+}
+
+void main() {
+  var list = [10.2,20.2,30.2];
+  var shoppingCart = ShoppingCart();
+  shoppingCart.prices = list;
+  print(shoppingCart.prices);
+}
