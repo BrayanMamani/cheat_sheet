3c73fba6a48d2b0602f9f9d99418d00c7c16afe8
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-05-16 00:00:03 -0400

    Redirecting constructors

---
+++ bin/redirecting_constructors.dart
@@ -0,0 +1,15 @@
+//Challenge
+class Color {
+  int red;
+  int green;
+  int blue;
+
+  Color(this.red, this.green, this.blue);
+
+  Color.black() : this(0, 0, 0);
+}
+
+void main(){
+  var color = Color.black();
+  print('red: ${color.red} - green: ${color.green} - blue: ${color.blue}');
+}
