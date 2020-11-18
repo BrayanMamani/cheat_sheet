4eedebbae194a8bfbd35578f09a266576ed13710
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-11-18 12:53:41 -0400

    Improve string interpolation

--- bin/string_interpolation.dart
+++ bin/string_interpolation.dart
@@ -1,15 +1,15 @@
-void main(){
+void main() {
   //Identifiers
   var number = 10.0;
   print('$number');
 
   //Expressions
-  print('${3+2}');
+  print('${3 + 2}');
   print('${'word'.toUpperCase()}');
 
   print(stringify(2, 3));
 }
 
-String stringify(int x, int y){
+String stringify(int x, int y) {
   return '$x $y';
 }
