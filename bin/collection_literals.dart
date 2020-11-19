d4a5004857d2391260d52d9245e88dd270fd0b00
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-11-19 14:22:15 -0400

    Update collection literals

--- bin/collection_literals.dart
+++ bin/collection_literals.dart
@@ -1,34 +1,34 @@
-void main(){
+void main() {
   //Inference type
   final aListOfStrings = ['one', 'two', 'three'];
   final aSetOfStrings = {'one', 'two', 'three'};
   final aMapOfStringsToInts = {
-    'one' : 1,
-    'two' : 2,
-    'three' : 3,
+    'one': 1,
+    'two': 2,
+    'three': 3,
   };
   //Defined type
   final aListOfInts = <int>[];
   final aSetOfInts = <int>{};
   final aMapOfIntToDouble = <int, double>{};
   //Specific
-  final aListOfBaseType = <DateTime>[DateTime(2020),DateTime(1010)];
+  final aListOfBaseType = <DateTime>[DateTime(2020), DateTime(1010)];
 
   //Challenge
   challenge();
 }
 
-void challenge(){
+void challenge() {
   //Defined
   final aListOfString = ['a', 'b'];
   final aSetOfInts = {3, 4, 5};
   final aMapOfStringsToInts = {
-    'myKey' : 12,
+    'myKey': 12,
   };
   //Not defined
   final anEmptyListOfDouble = <double>[];
   final anEmptySetOfString = <String>{};
-  final anEmptyMapOfDoublesToInts = <double, int> {};
+  final anEmptyMapOfDoublesToInts = <double, int>{};
   //Print defined
   print(aListOfString);
   print(aSetOfInts);
