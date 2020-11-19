2190bc65b318f11bd2bcdbc1007f723262144807
Brayan Mamani <brayanmamani.dev@gmail.com>
2020-11-19 14:15:03 -0400

    Refactor conditional property access

--- bin/conditional_property_access.dart
+++ bin/conditional_property_access.dart
@@ -1,11 +1,11 @@
-void main(){
+void main() {
   //Declare a variable
   var language = 'Dart';
   //Print date of variable
   print(language.toUpperCase());
 }
 
-String upperCaseIT(String str){
+String upperCaseIT(String str) {
   //Protection null
   return str?.toUpperCase();
 }
